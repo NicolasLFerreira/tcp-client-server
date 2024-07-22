0db83ffca111f405c042c9aeaec90b1b2001d584
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2024-07-22 23:25:05 +1200

    Added class with all the end point info

---
+++ NetworkTestfield/Configurator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace NetworkTestfield
+{
+    public class Configurator
+    {
+        public static readonly string IPPlain = "192.168.56.1";
+        public static readonly IPAddress IP = IPAddress.Parse(IPPlain);
+        public static readonly int Port = 13000;
+        public static readonly IPEndPoint EndPoint = new(IP, Port);
+    }
+}
--- NetworkTestfield/Program.cs
+++ NetworkTestfield/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+
+namespace NetworkTestField
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the Name of HOST   
+            string hostName = Dns.GetHostName();
+            Console.WriteLine(hostName);
+
+            // Get the IP from GetHostByName method of dns class. 
+            string IP = Dns.GetHostEntry(hostName).AddressList[0].ToString();
+            Console.WriteLine("IP Address is : " + IP);
+        }
+    }
+}
